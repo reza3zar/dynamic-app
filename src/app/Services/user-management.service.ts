44c3ff2b9771944d6afbbba11cbc1f91a4751920
reza3zar <younesi.nima@gmail.com>
2018-12-02 08:20:40 +0330

    Read ME

--- src/app/Services/user-management.service.ts
+++ src/app/Services/user-management.service.ts
@@ -11,7 +11,7 @@ export class UserManagementService {
   constructor(private http:HttpClient) { }
 
   public getUsersCollection():Observable<CustomControl[]>{
-    return this.http.get<CustomControl[]>("http://www.mocky.io/v2/5c0247f13500002a00ad0ae6");
+    return this.http.get<CustomControl[]>("http://www.mocky.io/v2/5c0255903500006a00ad0af1");
   }
 }
 
